b1291e55b4d1816a3a5ad854d2c44bf2950158ba
srmkimkc <104980811+srmkimkc@users.noreply.github.com>
2022-05-12 05:31:24 +0530

    Create traceroutecmd.java

---
+++ CODE FOR SIMULATING PING AND TRACEROUTE COMMANDS/traceroutecmd.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+public class traceroutecmd
+{
+     public static void runSystemCommand(String command)
+     {
+          try
+          {
+              Process p = Runtime.getRuntime().exec(command);
+	DataInputStream inputstream=new DataInputStream(p.getInputStream());
+              
+
+              String s = "";
+              while ((s = inputstream.readLine()) != null)
+                   System.out.println(s);
+          }
+          catch (Exception e)
+          {
+          }
+     }
+
+     public static void main(String[] args)
+     {  
+          String ip = "www.google.co.in";
+        
+        
+          runSystemCommand("tracert " + ip);
+     }
+}
